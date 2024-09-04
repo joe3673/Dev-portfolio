a3050f9686d75b8e15899ee96ebb99164694fccd
joseph <joseph.baker@mykenzie.snhu.edu>
2024-09-03 19:07:03 -0600

    Updated Address

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Joseph Baker. I am a professional and enthusiastic programmer with a passion for learning and exploring new technologies. I excel in problem-solving and have a self-motivated approach to learning. I enjoy working with almost all aspects of web application development and aim to make the web more accessible. My core skills are in Java, JavaScript, C#, and Python. I am open to job opportunities that align with my skills and interests.",
   email: 'JosephBakerSE@Gmail.com',
   phone: '(347)731-4290',
-  address: 'Colorado, United States',
+  address: 'United States',
   github: 'https://github.com/Joe3673',
   facebook: '',
   linkedIn: 'https://linkedin.com/in/joseph-baker-linkme',
